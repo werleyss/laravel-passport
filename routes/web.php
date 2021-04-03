3e354b51ff804ccc9732dcc6436ec146f8799566
werley <werley_desouzasilva@hotmail.com>
2021-04-03 10:35:58 -0300

    Configurando a laravel passport para auth de apis

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,28 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $query = http_build_query([
+        'client_id'     => '2',
+        'redirect_uri'  => 'http://localhost:9999/callback',
+        'response_type' => 'code',
+        'scope'         => ''
+    ]);
+
+    return redirect("http://localhost:8000/oauth/authorize?$query");
+});
+
+Route::get('callback', function (Request $request){
+    $code = $request->get('code');
+
+    $http = new \GuzzleHttp\Client();
+
+    $response = $http->post('http//localhost:8000/oauth/token', [
+        'form_params'   => [
+            'client_id'         => '2',
+            'client_secret'     => 'tkAo4kWWyMrf5qM8EfQLV2p5xDiJoMcI0yIU71NR',
+            'redirect_uri'      => 'http://localhost:9999/callback',
+            'code'              => $code,
+            'grant_type'        => 'authorization_code'
+        ]
+    ]);
 });
